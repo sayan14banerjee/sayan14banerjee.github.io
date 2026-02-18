3e6279be7afc59205f4cbc9fb946b557b4523ae6
Sayan Banerjee <119781497+sayan14banerjee@users.noreply.github.com>
2026-02-18 17:04:11 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -58,7 +58,7 @@ export default function Hero() {
         </div>
         </div>
         <div className="hero-image">
-      <img href="/public/portfolio.image.png" alt="Sayan Banerjee" />
+      <img src="/public/portfolio.image.png" alt="Sayan Banerjee" />
     </div>
       </div>
 
